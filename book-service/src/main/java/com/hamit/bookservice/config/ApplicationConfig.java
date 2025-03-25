99624a648a342b9b8f1c7ade0aa0a04e3d11dfa3
De3ph <05190000108@ogrenci.ege.edu.tr>
2025-03-25 23:18:34 +0300

    feat: conditional redis config

--- book-service/src/main/java/com/hamit/bookservice/config/ApplicationConfig.java
+++ book-service/src/main/java/com/hamit/bookservice/config/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package com.hamit.bookservice.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,5 +14,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
+@EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class ApplicationConfig {
 }
